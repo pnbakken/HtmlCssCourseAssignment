1a031710791dbb21d0d38070b8b3a6c0bdc8a9c4
pnbakken <74681160+pnbakken@users.noreply.github.com>
2021-10-07 14:05:18 +0200

    added search button to enable search for everyone on devices without 'enter' keys (basically everyone)

--- js/script.js
+++ js/script.js
@@ -5,6 +5,13 @@ searchField.addEventListener("keyup", (event) => {
  if (event.keyCode === 13 && searchField.value.trim()) {
      console.log("Enter entered");
      event.preventDefault();
-     location.href = `../collection.html?search_term=${searchField.value.trim().toLowerCase()}`;
+     location.href = `./collection.html?search_term=${searchField.value.trim().toLowerCase()}`;
  }
 });
+
+const searchButton = document.querySelector("#search-button");
+searchButton.onclick = () => {
+    if (searchField.value !== "") {
+        searchButton.href += "?search_term=" + searchField.value.trim().toLowerCase();
+    }
+}
