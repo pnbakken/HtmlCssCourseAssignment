ed849a72a52b6c4e61f27cc14f181a0f1a784ee7
pnbakken <74681160+pnbakken@users.noreply.github.com>
2021-10-28 13:58:34 +0200

    added criteria filter and square eyes url to index page movie lists

--- js/fetch-list.js
+++ js/fetch-list.js
@@ -2,43 +2,62 @@ const TMDB_API_KEY = "d5b5096de95f26903a3b6601c9a24d4f";
 const TMDB_URL = "https://api.themoviedb.org/3/";
 const TMDB_IMG_URL = "https://image.tmdb.org/t/";
 
+const SE_API_URL = "https://www.plumtree.no/square-eyes-api/wp-json/wc/store/products";
+
+const genres = {
+    drama : "17",
+    action : "19",
+    comedy : "21",
+    horror : "22",
+}
+
 const scroller1 = document.querySelector("#scroller-items-1");
 const scroller2 = document.querySelector("#scroller-items-2");
 const scroller3 = document.querySelector("#scroller-items-3");
 
-getList("with_genres=18&primary_release_year=2021", scroller1);
-getList("with_genres=28", scroller2);
-getList("with_genres=27", scroller3)
+getList("categories", genres.drama, scroller1);
+getList("categories", genres.action, scroller2);
+getList("categories", genres.horror, scroller3)
 
-async function getList(listString, target) {
-    const url = TMDB_URL + "discover/movie/?api_key=" + TMDB_API_KEY + "&" + listString;
+async function getList(criteria, sign, target) {
+    const url = SE_API_URL; 
     
     try {
         const response = await fetch(url);
         const result = await response.json();
         console.log(result);
-        generateList(target, result);
+        const criteriaMatches = result.filter( (item) => matchAnyCriteria(item, criteria, sign));
+        console.log(criteriaMatches);
+        generateList(target, criteriaMatches);
     } catch (err) {
         console.error(err);
     }
 }
 
+function matchAnyCriteria(item, criteria, sign) {
+    if (item[criteria]) {
+        for (let crit of item[criteria]) {
+            console.log( crit + " === " + sign);
+            if (crit.id == sign) {
+                return true;
+            }
+        }
+    } else return false;
+}
+
 function generateList(target, list) {
     let count = 0;
     target.innerHTML ="";
-    for (let item of list.results) {
+    for (let item of list) {
         if (count === 6) {
             break;
         }
         target.innerHTML += `<div class="index-list-item">
                                 <a href="./film-page.html?movie_id=${item.id}">    
-                                    <img src="${getPosterUrl(item.poster_path)}" alt="${item.title} poster" />
+                                    <img src="${item.images[0].src}" alt="${item.name} poster" />
                                 </a>
                             </div>`;
         count++;
     }
 }
 
-function getPosterUrl(url) {
-    return TMDB_IMG_URL + "p/w500/" + url;
-}
